7de5c78b656b72f8910ec225fc66a1530a462893
nguyenvukhang <brew4k@gmail.com>
2025-11-28 12:27:09 +0800

    Update

--- src/filesystem.rs
+++ src/filesystem.rs
@@ -1,6 +1,35 @@
 use std::path::{Path, PathBuf};
 use std::{fs, io};
 
+#[allow(unused)]
+fn is_lakefile(filepath: &Path) -> bool {
+    if let Some("lakefile.toml" | "lakefile.lean") = filepath.to_str() {
+        return true;
+    }
+    false
+}
+
+fn find_lake_root(cwd: &Path) -> Option<PathBuf> {
+    let mut cursor = cwd.to_path_buf();
+    let mut i: u8 = 64; // maximum number of `cd ..`s.
+    while i > 0 {
+        i -= 1;
+        for filename in ["lakefile.lean", "lakefile.toml"] {
+            cursor.push(filename);
+            let metadata = fs::metadata(&cursor);
+            cursor.pop();
+            match metadata {
+                Ok(m) if m.is_file() => return Some(cursor),
+                _ => {}
+            }
+        }
+        if !cursor.pop() {
+            break;
+        }
+    }
+    None
+}
+
 /// A cached filesytem manager.
 pub struct FilesystemManager {
     /// Absolute path to current directory (cached).
@@ -28,36 +57,20 @@ impl FilesystemManager {
     /// The first parent directory (relative to CWD) that contains `lakefile.lean`.
     pub fn absolute_lake_root(&mut self) -> PathBuf {
         if let None = self.absolute_lake_root {
-            self.absolute_lake_root =
-                Some(first_parent_that_contains(&self.cwd(), "lakefile.lean"));
+            self.absolute_lake_root = find_lake_root(&self.cwd())
         }
-        self.absolute_lake_root.as_ref().unwrap().to_path_buf()
+        let root = self.absolute_lake_root.as_ref();
+        root.expect("Unable to locate lake root.").to_path_buf()
     }
-}
-
-/// Gets the the absolute path of the first parent directory that contains a
-/// particular file. Search upwards from the `cwd` parameter.
-fn first_parent_that_contains(cwd: &Path, filename: &str) -> PathBuf {
-    let mut buf = cwd.join(filename);
-    let mut i: u8 = 9;
-    while i > 0 {
-        i -= 1;
-        let Ok(m) = fs::metadata(&buf) else {
-            buf.pop();
-            buf.set_file_name(filename);
-            continue;
-        };
-        if m.is_file() {
-            buf.pop();
-            return buf;
-        }
-    }
-    panic!("{filename} not found in parent dirs.");
 }
 
 /// Recursively walks through the contents of a directory. Ignores a
 /// fixed set of directories.
-pub fn walk<F>(root: &Path, mut action: F, ignore: &[&str]) -> io::Result<()>
+pub fn walk<F>(
+    root: &Path,
+    mut action: F,
+    ignore_dirs: &[&str],
+) -> io::Result<()>
 where
     F: FnMut(PathBuf) -> (),
 {
@@ -75,7 +88,7 @@ where
             else {
                 continue;
             };
-            if ignore.contains(&filename) {
+            if ignore_dirs.contains(&filename) {
                 continue;
             }
             for dir_ent in fs::read_dir(dir)? {
@@ -87,9 +100,12 @@ where
     }
 }
 
+/// Recursively obtains all the files with extension "lean" under `root`, while
+/// ignoring directories that are contained in `ignore_dirs`. Also, ignores
+/// "lakefile.lean".
 pub fn get_lean_files_in_dir(
     root: &Path,
-    ignore: &[&str],
+    ignore_dirs: &[&str],
 ) -> io::Result<Vec<PathBuf>> {
     let mut lean_files = vec![];
     walk(
@@ -103,7 +119,7 @@ pub fn get_lean_files_in_dir(
                 lean_files.push(file)
             }
         },
-        ignore,
+        ignore_dirs,
     )?;
     Ok(lean_files)
 }
